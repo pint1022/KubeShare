969e31051d47d395a6c93a75efa3bf84de59241b
steven w <xu.s.wang@gmail.com>
2022-05-17 14:38:29 -0700

    remove error codes

--- docker/alnr-exporter/run-multigpus.sh
+++ docker/alnr-exporter/run-multigpus.sh
@@ -24,7 +24,7 @@ port=49901
 _list=$(nvidia-smi --format=csv,noheader --query-gpu=uuid)
 count=0
 for gpu in $_list; do
-    echo 0 > $1/$gpu
+    # echo 0 > $1/$gpu
     # python3 /launcher.py /gem-schd /gem-pmgr $gpu $1/$gpu $2 --port $port 1>&2 &
     # pids="$pids $!"
     port=$(($port+1))
